5a72e3feded6a7d373dcc55df9244570948a61d0
thefordz <amthefordz@gmail.com>
2024-04-16 21:52:32 +0700

    add curve line

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,50 @@
-import { Float, OrbitControls } from "@react-three/drei";
+import { Float, Line, OrbitControls } from "@react-three/drei";
 import { Background } from "./Background";
 import { Airplane } from "./Airplane";
 import { Cloud } from "./Cloud";
+import { useMemo } from "react";
+
+import * as THREE from "three";
+
+const LINE_NB_POINTS = 42;
 
 export const Experience = () => {
+  const curve = useMemo(() => {
+    return new THREE.CatmullRomCurve3(
+      [
+        new THREE.Vector3(0, 0, 0),
+        new THREE.Vector3(0, 0, -10),
+        new THREE.Vector3(-2, 0, -20),
+        new THREE.Vector3(-3, 0, -30),
+        new THREE.Vector3(0, 0, -40),
+        new THREE.Vector3(5, 0, -50),
+        new THREE.Vector3(7, 0, -60),
+        new THREE.Vector3(5, 0, -70),
+        new THREE.Vector3(0, 0, -80),
+        new THREE.Vector3(0, 0, -90),
+        new THREE.Vector3(0, 0, -100),
+      ],
+      false,
+      "catmullrom",
+      0.5,
+    );
+  }, []);
+
+  const linePoints = useMemo(() => {
+    return curve.getPoints(LINE_NB_POINTS);
+  }, [curve]);
+
+  const shape = useMemo(() => {
+    const shape = new THREE.Shape();
+    shape.moveTo(0, -0.2);
+    shape.lineTo(0, 0.2);
+
+    return shape;
+  }, [curve]);
+
   return (
     <>
-      <OrbitControls />
+      <OrbitControls enableZoom={false} />
       <Background />
       <Float floatIntensity={2} speed={2}>
         <Airplane
@@ -15,6 +53,23 @@ export const Experience = () => {
           position-y={0.1}
         />
       </Float>
+
+      <group position-y={-2}>
+        <mesh>
+          <extrudeGeometry
+            args={[
+              shape,
+              {
+                steps: LINE_NB_POINTS,
+                bevelEnabled: false,
+                extrudePath: curve,
+              },
+            ]}
+          />
+          <meshStandardMaterial color={"white"} opacity={0.7} transparent />
+        </mesh>
+      </group>
+
       <Cloud opacity={0.5} scale={[0.3, 0.3, 0.3]} position={[-2, 1, -3]} />
       <Cloud opacity={0.5} scale={[0.2, 0.3, 0.4]} position={[1.5, -0.5, -2]} />
       <Cloud
